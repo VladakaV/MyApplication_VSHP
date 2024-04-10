7e26a081f3d0b0348a745a8ec365f5073544623c
vlada.v2014@mail.ru <vlada.v2014@mail.ru>
2024-04-10 18:26:48 +0300

    Передача данных с одной страницы на другую + Person

---
+++ app/src/main/java/com/example/myapplication/Person.java
@@ -0,0 +1,37 @@
+package com.example.myapplication;
+
+import java.io.Serializable;
+
+public class Person  implements Serializable {
+    String name;
+    int age;
+    String surname;
+    public Person(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+}
